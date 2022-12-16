995068712a34b9c25d3e4a4b8252dc7c93b4ccd8
Gabryel <gabryelmaraujo@gmail.com>
2022-12-16 16:57:48 -0300

    feat: register page with mobile styles

---
+++ hamburgueriav2/src/Components/RegisterForm/index.tsx
@@ -0,0 +1,29 @@
+import RegisterFormStyles from "./styles"
+
+
+const RegisterForm = () => {
+
+return(
+    <RegisterFormStyles>
+        <label htmlFor="registerName" className="labelRegisterName">
+            <input type="text" name="registerName" id="registerName" />
+            <span className="registerNameSpan">Nome</span>
+        </label>
+        <label htmlFor="registerEmail" className="labelRegisterEmail">
+            <input type="email" name="registerEmail" id="registerEmail" />
+            <span className="registerEmailSpan">Email</span>
+        </label>
+        <label htmlFor="registerPassword" className="labelRegisterPassword">
+            <input type="password" name="registerPassword" id="registerPassword" />
+            <span className="registerPasswordSpan">Senha</span>
+        </label>
+        <div className="submitDiv">
+            <button type="submit" className="registerSubmitBttn">Cadastrar</button>
+        </div>
+
+    </RegisterFormStyles>
+)
+
+}
+
+export default RegisterForm
